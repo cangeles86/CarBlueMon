599a30fc33b0a6a47487283388c1144b5c435fdc
cangeles86 <cangeles86@hotmail.com>
2014-03-09 17:22:35 -0500

    Get Latitude and Longitude Test

--- SanPablo.CarBluMon.Mobile/src/com/sanpablo/carblumon/mobile/MainActivity.java
+++ SanPablo.CarBluMon.Mobile/src/com/sanpablo/carblumon/mobile/MainActivity.java
@@ -1,16 +1,67 @@
 package com.sanpablo.carblumon.mobile;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	Button currentLocation;	
+	TextView message;
+	double latitude;
+	double longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        LocationListener mlocListener = new LocationListener() {			
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+				// TODO Auto-generated method stub
+								
+			}
+			
+			@Override
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderDisabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onLocationChanged(Location location) {
+				// TODO Auto-generated method stub
+				latitude = location.getLatitude();
+				longitude = location.getLongitude();
+			}
+		};
+		
+		mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER , 0, 0, mlocListener);
+		
+        currentLocation = (Button)findViewById(R.id.getLocation);
+        message = (TextView)findViewById(R.id.message);
+        currentLocation.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				message.setText(String.valueOf(latitude));
+			}
+		});
+   }
 
 
     @Override
