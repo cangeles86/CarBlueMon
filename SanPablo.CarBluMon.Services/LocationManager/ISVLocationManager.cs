2a9a72430a4847389ab5556ca96f175c2c9b9645
cangeles86 <cangeles86@hotmail.com>
2014-03-09 17:42:51 -0500

    Create Service Create Data Location Create Repository Pattern

---
+++ SanPablo.CarBluMon.Services/LocationManager/ISVLocationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SanPablo.CarBluMon.Services.LocationManager
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISVLocationManager" in both code and config file together.
+    [ServiceContract]
+    public interface ISVLocationManager
+    {
+        [OperationContract]
+        bool RegisterLocation (double Latitude, double Longitud);
+    }
+}
---
+++ SanPablo.CarBluMon.Services/LocationManager/SVLocationManager.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SanPablo.CarBluMon.Services.LocationManager
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SVLocationManager" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SVLocationManager.svc or SVLocationManager.svc.cs at the Solution Explorer and start debugging.
+    public class SVLocationManager : ISVLocationManager
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
