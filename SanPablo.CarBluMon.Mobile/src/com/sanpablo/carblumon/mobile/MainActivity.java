4128d85502852009cfc63e29c641926c988b4c1d
cangeles86 <cangeles86@hotmail.com>
2014-03-14 19:31:03 -0500

    Mobile Interfaces

--- SanPablo.CarBluMon.Mobile/src/com/sanpablo/carblumon/mobile/MainActivity.java
+++ SanPablo.CarBluMon.Mobile/src/com/sanpablo/carblumon/mobile/MainActivity.java
@@ -1,16 +1,20 @@
 package com.sanpablo.carblumon.mobile;
 
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import com.sanpablo.carblumon.service.WebServices;
+
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+
 
 public class MainActivity extends Activity {
 
@@ -22,43 +26,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        LocationListener mlocListener = new LocationListener() {			
-			@Override
-			public void onStatusChanged(String provider, int status, Bundle extras) {
-				// TODO Auto-generated method stub
-								
-			}
-			
-			@Override
-			public void onProviderEnabled(String provider) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onProviderDisabled(String provider) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onLocationChanged(Location location) {
-				// TODO Auto-generated method stub
-				latitude = location.getLatitude();
-				longitude = location.getLongitude();
-			}
-		};
-		
-		mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER , 0, 0, mlocListener);
-		
+        //Get Objects Values
         currentLocation = (Button)findViewById(R.id.getLocation);
         message = (TextView)findViewById(R.id.message);
+        
+        
+		
         currentLocation.setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				message.setText(String.valueOf(latitude));
+				Location lo = GetLocation();
+				WebServices ws = new WebServices();
+				ws.SendLocation(1, 1);
+				String message1;
+				if (lo == null) {
+					message1 = "Error";
+				}
+				else {
+					message1 = "Bien";
+				}
+		        Toast.makeText(getApplicationContext(), message1 , Toast.LENGTH_SHORT).show();
+				//WebServices services= new WebServices();
+				//services.SendLocation(latitude, longitude);
 			}
 		});
    }
@@ -69,6 +59,29 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    private Location GetLocation()
+    {
+    	Location lastLocation;
+    	
+    	LocationManager mlocManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy( Criteria.ACCURACY_COARSE );
+        String provider = mlocManager.getBestProvider( criteria, true );
+
+        if ( provider == null ) {
+            //Log.e( TAG, "No location provider found!" );
+            lastLocation = null;
+        }
+        else {
+        	lastLocation = mlocManager.getLastKnownLocation(provider);
+            latitude = lastLocation.getLatitude();
+            longitude = lastLocation.getLongitude();
+		}
+        
+        return lastLocation;
     }
     
 }
