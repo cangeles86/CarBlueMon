6c72b3ca943dfd85dc742cd3c138d401e4b6fe3c
cangeles86 <cangeles86@hotmail.com>
2014-03-17 10:33:03 -0500

    Change Services SOAP to RestFull

---
+++ SanPablo.CarBluMon.Services/Medicine/IMedicine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SanPablo.CarBluMon.Services.Medicine
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMedicine" in both code and config file together.
+    [ServiceContract]
+    public interface IMedicine
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
