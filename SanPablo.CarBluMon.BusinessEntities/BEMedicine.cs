4ee055a1ef28c10be3231b7d9ef6ade8ed329394
cangeles86 <cangeles86@hotmail.com>
2014-03-21 21:45:18 -0500

    Añadir Entidades de Negocio

---
+++ SanPablo.CarBluMon.BusinessEntities/BEMedicine.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace SanPablo.CarBluMon.BusinessEntities
+{
+    [DataContract]
+    public class BEMedicine
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public BEUser User { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public BEDoctor Doctor { get; set; }
+
+    }
+}
